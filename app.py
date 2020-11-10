1a39ede90b91cb429307ae3d1275133be57ef971
agajareiitr <agajare@ch.iitr.ac.in>
2020-11-10 14:16:36 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/<string:n>")
+
+def getoptionchain(n):
+    url = "https://www.nseindia.com/api/option-chain-equities?symbol="+n
+    headers = {"User-Agent": "Mozilla/5.0"}
+    with requests.Session() as ssn:
+        ssn.get('https://www.nseindia.com/',headers=headers)
+        data = ssn.get(url,headers=headers).json()
+    
+    return data
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
